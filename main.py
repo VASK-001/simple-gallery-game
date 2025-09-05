98ea8d073e387592bbe301ee166ea95da1026bf9
VASK-001 <vadim.skrygin@gmail.com>
2025-09-05 22:58:22 +0300

    ver 0 - работает фон, цель, распозн ЛКМ, перенос цели + 2 звука

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ pygame.display.set_caption("Тир")
 icon = pygame.image.load("img/icon.jpg")
 pygame.display.set_icon(icon)
 
+# Загрузка звуков
+hit_sound = pygame.mixer.Sound("sound/hit.wav")  # попаданиe
+miss_sound = pygame.mixer.Sound("sound/miss.wav")  # промах
+
 target_img = pygame.image.load("img/trg3.png")
-target_width = 70
-target_height = 70
+target_width = 80
+target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
@@ -28,9 +32,13 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:  # Перенесено внутрь цикла обработки событий
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Проверка попадания
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                hit_sound.play()  # Воспроизводим звук попадания
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            else:
+                miss_sound.play()  # Воспроизводим звук промаха
 
     # Отрисовка
     screen.fill(color)
