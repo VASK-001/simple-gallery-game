bc92788da9caa836aec2c56d8fbadec38c99b87e
VASK-001 <vadim.skrygin@gmail.com>
2025-09-05 15:15:24 +0300

    restored full code

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
+import random
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Тир")
+icon = pygame.image.load("img/icon.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/trg3.png")
+target_width = 70
+target_height = 70
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = 1
 while running:
     pass
+
 
 
 
