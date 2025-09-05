b81a3dfb30a0ad3c276da168adbad250353c5023
VASK-001 <vadim.skrygin@gmail.com>
2025-09-05 16:03:09 +0300

    Just the frame

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ import pygame
 import random
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption("Тир")
-icon = pygame.image.load("img/icon.jpg")
-pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/trg3.png")
 target_width = 70
